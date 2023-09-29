455b6458913848380fc946466369dcf9e44b1ec8
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-09-29 23:57:55 +0200

    Update

--- app/models.py
+++ app/models.py
@@ -1,2 +1,8 @@
 from flask_mongoengine import Document
 from mongoengine.fields import StringField, IntField
+
+class Book(Document):
+    title = StringField(required=True)
+    author = StringField(required=True)
+    genre = StringField()
+
